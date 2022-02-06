ae9ffdbb8d6740471889f870805f04059346c778
Ahmed ibrahim <89364017+Ahmedhassanin12@users.noreply.github.com>
2022-02-06 11:06:55 +0200

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,6 @@
+let shereBtn = document.querySelector(".small-box svg");
+
+let hiddenText = document.querySelector(".ahmed");
+shereBtn.addEventListener("click", function () {
+  hiddenText.classList.toggle("hidden");
+});
